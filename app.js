39f20924e009f47048fead457cd7fa3cb777fbc4
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-03 13:53:14 +0000

    Added request library

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const request = require('request');
 
 const port = process.env.PORT || 3000;
 
@@ -12,3 +13,16 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
+
+// const url = 'http://api.openweathermap.org/data/2.5/weather?q=london';
+// const units = '&units=metric';
+// const token = `&appid=${process.env.OPEN_WEATHER_MAP_KEY}`;
+
+// request(url + units + token, (err, res, body) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     let data = JSON.parse(body);
+//     console.log(Math.round(data.main.temp));
+//   }
+// });
