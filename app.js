5fe280332cf41fcbffebe7547b52d49869311656
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-04 12:35:38 +0000

    Minor refactor

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const axios = require('axios').default
+const axios = require('axios').default;
 
 const port = process.env.PORT || 3000;
 
